75cd4eeb1741365697ac165796ece5e0d2d9de36
Anthony Njeri <anthonynwaweru@gmail.com>
2026-04-28 10:40:44 +0200

    UPDATE feat: Comment Schema, Likes Schema

---
+++ src/_utils/translator/deepl-language.client.ts
@@ -0,0 +1,16 @@
+import { DeepLClient } from 'deepl-node';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DeeplLanguageTranslator {
+  private authKey = '2de04f5d-9e61-45bc-8e53-fd6d69817aad:fx';
+  constructor(private deeplClient: DeepLClient) {
+    this.deeplClient = new DeepLClient(this.authKey);
+  }
+
+  async translateTest(text: string) {
+    const result = await this.deeplClient.translateText(text, null, 'fr');
+
+    return result;
+  }
+}
---
+++ src/comments/schemas/comments.schema.ts
@@ -0,0 +1,30 @@
+import mongoose, { HydratedDocument, Types } from 'mongoose';
+import { PostDocument } from '../posts/schemas/post.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { UserDocument } from '../users/schemas/users.schema';
+import e from 'express';
+
+export type CommentsDocument = HydratedDocument<Comments>;
+@Schema({ timestamps: true })
+export class Comments {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  })
+  post: Types.ObjectId | PostDocument;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  author: Types.ObjectId | UserDocument;
+
+  @Prop({
+    required: true,
+  })
+  comment: string;
+}
+
+export const CommentsSchema = SchemaFactory.createForClass(Comments);
